c59289dc2daec0e8f17536d812d8436c47e55fc9
田风有 <420144542@qq.com>
2019-11-02 19:46:52 +0800

    添加View弹出框

--- TFY_ProgressHUD/Controller/ViewController.m
+++ TFY_ProgressHUD/Controller/ViewController.m
@@ -10,7 +10,7 @@
 #import "TFY_ProgressHUD.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIView *contentView;
 @end
 
 @implementation ViewController
@@ -18,11 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.contentView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 300, 500)];
+    self.contentView.backgroundColor = [UIColor cyanColor];
+    
     self.view.backgroundColor = [UIColor cyanColor];
-    NSArray *arr = @[@"文字提示",@"文字加枚举类型",@"枚举类型",@"展示成功的状态",@"成功加时间设计",@"失败提示",@"失败加时间",@"提示",@"提示加时间"];
+    NSArray *arr = @[@"文字提示",@"文字加枚举类型",@"枚举类型",@"展示成功的状态",@"成功加时间设计",@"失败提示",@"失败加时间",@"提示",@"提示加时间",@"淡入淡出",@"收缩",@"底部弹出",@"顶部弹出",@"中心弹出"];
     for (NSInteger i = 0; i<arr.count; i++) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        btn.frame = CGRectMake(0, 64+i*60, [UIScreen mainScreen].bounds.size.width, 50);
+        btn.frame = CGRectMake(0, 40+i*60, [UIScreen mainScreen].bounds.size.width, 50);
         btn.backgroundColor = [UIColor orangeColor];
         btn.titleLabel.font = [UIFont systemFontOfSize:14];
         btn.tag = i;
@@ -39,11 +42,11 @@
         [self performSelector:@selector(hpv) withObject:nil afterDelay:1.5];
     }
     else if (btn.tag==1){
-        [TFY_ProgressHUD showWithStatus:@"提示" maskType:ProgressHUDMaskTypeGradient];
+        [TFY_ProgressHUD showWithStatus:@"提示" maskType:PopupMaskType_Gradient];
         [TFY_ProgressHUD dismissWithError:@"阿噶几发件方；阿发" afterDelay:1.3];
     }
     else if (btn.tag==2){
-        [TFY_ProgressHUD showWithMaskType:ProgressHUDMaskTypeNone];
+        [TFY_ProgressHUD showWithMaskType:PopupMaskType_None];
         [TFY_ProgressHUD dismissWithSuccess:@"关闭" afterDelay:1.5];
     }
     else if (btn.tag==3){
@@ -64,8 +67,27 @@
     else if (btn.tag==8){
         [TFY_ProgressHUD showPromptWithStatus:@"就会发回复拉回来发了疯" duration:1];
     }
-    
-    
+    else if (btn.tag==9){
+        TFY_ProgressHUD *popView = [TFY_ProgressHUD popupWithContentView:self.contentView showType:PopupShowType_FadeIn dismissType:PopupDismissType_FadeOut maskType:PopupMaskType_Clear dismissOnBackgroundTouch:NO dismissOnContentTouch:YES];
+        [popView show];
+        
+    }
+    else if (btn.tag==10){
+        TFY_ProgressHUD *popView = [TFY_ProgressHUD popupWithContentView:self.contentView showType:PopupShowType_ShrinkIn dismissType:PopupDismissType_ShrinkOut maskType:PopupMaskType_None dismissOnBackgroundTouch:YES dismissOnContentTouch:YES];
+        [popView showWithDuration:2];
+    }
+    else if (btn.tag==11){
+        TFY_ProgressHUD *popView = [TFY_ProgressHUD popupWithContentView:self.contentView showType:PopupShowType_SlideInFromBottom dismissType:PopupDismissType_SlideOutToBottom maskType:PopupMaskType_None dismissOnBackgroundTouch:YES dismissOnContentTouch:YES];
+        [popView showWithDuration:2];
+    }
+    else if (btn.tag==12){
+        TFY_ProgressHUD *popView = [TFY_ProgressHUD popupWithContentView:self.contentView showType:PopupShowType_SlideInFromTop dismissType:PopupDismissType_SlideOutToTop maskType:PopupMaskType_None dismissOnBackgroundTouch:YES dismissOnContentTouch:YES];
+        [popView showWithDuration:2];
+    }
+    else if (btn.tag==13){
+        TFY_ProgressHUD *popView = [TFY_ProgressHUD popupWithContentView:self.contentView showType:PopupShowType_BounceIn dismissType:PopupDismissType_BounceOut maskType:PopupMaskType_None dismissOnBackgroundTouch:YES dismissOnContentTouch:YES];
+           [popView showWithDuration:2];
+    }
     
 }
 
--- TFY_ProgressHUD/TFY_ProgressHUD/TFY_ProgressHUD.h
+++ TFY_ProgressHUD/TFY_ProgressHUD/TFY_ProgressHUD.h
@@ -10,14 +10,180 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef enum : NSUInteger{
-    ProgressHUDMaskTypeNone = 1,  // 当提示显示的时间，用户仍然可以做其他操作，比如View 上面的输入等
-    ProgressHUDMaskTypeClear,     // 用户不可以做其他操作
-    ProgressHUDMaskTypeBlack,     // 用户不可以做其他操作，并且背景色是黑色
-    ProgressHUDMaskTypeGradient   // 用户不可以做其他操作，并且背景色是渐变的
-}ProgressHUDMaskType;
+typedef NS_ENUM(NSUInteger, PopupShowType) {
+    PopupShowType_None,              //没有
+    PopupShowType_FadeIn,            //淡入
+    PopupShowType_GrowIn,            //成长
+    PopupShowType_ShrinkIn,           //收缩
+    PopupShowType_SlideInFromTop,     //从顶部，底部，左侧，右侧滑入
+    PopupShowType_SlideInFromBottom,
+    PopupShowType_SlideInFromLeft,
+    PopupShowType_SlideInFromRight,
+    PopupShowType_BounceIn,           //从顶部，底部，左侧，右侧，中心弹跳
+    PopupShowType_BounceInFromTop,
+    PopupShowType_BounceInFromBottom,
+    PopupShowType_BounceInFromLeft,
+    PopupShowType_BounceInFromRight
+};
+
+typedef NS_ENUM(NSUInteger, PopupDismissType) {
+    PopupDismissType_None,
+    PopupDismissType_FadeOut,
+    PopupDismissType_GrowOut,
+    PopupDismissType_ShrinkOut,
+    PopupDismissType_SlideOutToTop,
+    PopupDismissType_SlideOutToBottom,
+    PopupDismissType_SlideOutToLeft,
+    PopupDismissType_SlideOutToRight,
+    PopupDismissType_BounceOut,
+    PopupDismissType_BounceOutToTop,
+    PopupDismissType_BounceOutToBottom,
+    PopupDismissType_BounceOutToLeft,
+    PopupDismissType_BounceOutToRight
+};
+//在水平方向上布置弹出窗口
+typedef NS_ENUM(NSUInteger, PopupHorizontalLayout) {
+    PopupHorizontalLayout_Custom,
+    PopupHorizontalLayout_Left,
+    PopupHorizontalLayout_LeftOfCenter,           //中心左侧
+    PopupHorizontalLayout_Center,
+    PopupHorizontalLayout_RightOfCenter,
+    PopupHoricontalLayout_Right
+};
+//在垂直方向上布置弹出窗口
+typedef NS_ENUM(NSUInteger, PopupVerticalLayout) {
+    PopupVerticalLayout_Custom,
+    PopupVerticalLayout_Top,
+    PopupVerticalLayout_AboveCenter,              //中心偏上
+    PopupVerticalLayout_Center,
+    PopupVerticalLayout_BelowCenter,
+    PopupVerticalLayout_Bottom
+};
+
+typedef NS_ENUM(NSUInteger, PopupMaskType) {
+    //允许与底层视图交互
+    PopupMaskType_None,
+    //不允许与底层视图交互。
+    PopupMaskType_Clear,
+    //不允许与底层视图、背景进行交互。
+    PopupMaskType_Dimmed,
+    // 用户不可以做其他操作，并且背景色是黑色
+    PopupMaskType_Black,
+    // 用户不可以做其他操作，并且背景色是渐变的
+    PopupMaskType_Gradient
+};
+
+struct PopupLayout {
+    PopupHorizontalLayout horizontal;
+    PopupVerticalLayout vertical;
+};
+
+typedef struct PopupLayout PopupLayout;
+
+extern PopupLayout PopupLayoutMake(PopupHorizontalLayout horizontal, PopupVerticalLayout vertical);
+
+extern const PopupLayout PopupLayout_Center;
+
 
 @interface TFY_ProgressHUD : UIView
+/**
+ *   自定义视图
+ */
+@property (nonatomic, strong) UIView *contentView;
+/**
+ *  弹出动画
+ */
+@property (nonatomic, assign) PopupShowType showType;
+/**
+ *  消失动画
+ */
+@property (nonatomic, assign) PopupDismissType dismissType;
+/**
+ *  交互类型
+ */
+@property (nonatomic, assign) PopupMaskType maskType;
+/**
+ *  默认透明的0.5，通过这个属性可以调节
+ */
+@property (nonatomic, assign) CGFloat dimmedMaskAlpha;
+/**
+ *  提示透明度
+ */
+@property (nonatomic, assign) CGFloat toastMaskAlpha;
+/**
+ *  动画出现时间默认0.15
+ */
+@property (nonatomic, assign) CGFloat showInDuration;
+/**
+ *  动画消失时间默认0.15
+ */
+@property (nonatomic, assign) CGFloat dismissOutDuration;
+/**
+ *   当背景被触摸时，弹出窗口会消失。
+ */
+@property (nonatomic, assign) BOOL shouldDismissOnBackgroundTouch;
+/**
+ *  当内容视图被触摸时，弹出窗口会消失默认no
+ */
+@property (nonatomic, assign) BOOL shouldDismissOnContentTouch;
+/**
+ *   显示动画启动时回调。
+ */
+@property (nonatomic, copy, nullable) void(^willStartShowingBlock)(void);
+/**
+ *  显示动画完成启动时回调。
+ */
+@property (nonatomic, copy, nullable) void(^didFinishShowingBlock)(void);
+/**
+ *  显示动画将消失时回调。
+ */
+@property (nonatomic, copy, nullable) void(^willStartDismissingBlock)(void);
+/**
+ *  显示动画已经消失时回调。
+ */
+@property (nonatomic, copy, nullable) void(^didFinishDismissingBlock)(void);
+/**
+ *  背景视图
+ */
+@property (nonatomic, strong, readonly) UIView *backgroundView;
+/**
+ *  展现内容视图
+ */
+@property (nonatomic, strong, readonly) UIView *containerView;
+/**
+ *  是否开始展现
+ */
+@property (nonatomic, assign, readonly) BOOL isBeingShown;
+/**
+ *  正在展现
+ */
+@property (nonatomic, assign, readonly) BOOL isShowing;
+/**
+ *  开始消失
+ */
+@property (nonatomic, assign, readonly) BOOL isBeingDismissed;
+/**
+ *  弹出一个自定义视图
+ */
++ (TFY_ProgressHUD *)popupWithContentView:(UIView *)contentView;
+/**
+ *  弹出自定义文字提示框
+ */
++ (void)showToastVieWiththContent:(NSString *)content showType:(PopupShowType)showType dismissType:(PopupDismissType)dismissType stopTime:(NSInteger)time;
+/**
+ *  弹出自定义文字提示框
+ */
++ (void)showToastViewWithAttributedContent:(NSAttributedString *)attributedString showType:(PopupShowType)showType dismissType:(PopupDismissType)dismissType stopTime:(NSInteger)time;
+/**
+ *   弹出一个自定义视图
+ */
++ (TFY_ProgressHUD *)popupWithContentView:(UIView *)contentView showType:(PopupShowType)showType dismissType:(PopupDismissType)dismissType maskType:(PopupMaskType)maskType dismissOnBackgroundTouch:(BOOL)shouldDismissOnBackgroundTouch dismissOnContentTouch:(BOOL)shouldDismissOnContentTouch;
+/**
+ *  结束弹框
+ */
++ (void)dismissAllPopups;
++ (void)dismissPopupForView:(UIView *)view animated:(BOOL)animated;
++ (void)dismissSuperPopupIn:(UIView *)view animated:(BOOL)animated;
 /**
  *  不带任何文字的弹出框
  */
@@ -29,9 +195,9 @@ typedef enum : NSUInteger{
 /**
  *  展示状态  status   显示状态  maskType 枚举类型
  */
-+ (void)showWithStatus:(NSString*)status maskType:(ProgressHUDMaskType)maskType;
++ (void)showWithStatus:(NSString*)status maskType:(PopupMaskType)maskType;
 
-+ (void)showWithMaskType:(ProgressHUDMaskType)maskType;
++ (void)showWithMaskType:(PopupMaskType)maskType;
 /**
  *  展示成功的状态  string 传字符串
  */
@@ -73,10 +239,25 @@ typedef enum : NSUInteger{
 
 + (BOOL)isVisible;
 
-+ (TFY_ProgressHUD*)sharedView ;
++ (TFY_ProgressHUD*)sharedView;
 
 - (void)dismissWithNoAni;
 
+- (void)show;
+
+- (void)showWithLayout:(PopupLayout)layout;
+
+- (void)showWithDuration:(NSTimeInterval)duration;
+
+- (void)showWithLayout:(PopupLayout)layout duration:(NSTimeInterval)duration;
+
+- (void)showAtCenterPoint:(CGPoint)point inView:(UIView *)view;
+
+- (void)showAtCenterPoint:(CGPoint)point inView:(UIView *)view duration:(NSTimeInterval)duration;
+/**
+ *   取消所有提示
+ */
+- (void)dismissAnimated:(BOOL)animated;
 @end
 
 NS_ASSUME_NONNULL_END
